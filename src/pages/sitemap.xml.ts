73fdcae091456740e63c3eb3c13c97a8afc570d9
Professor-Tex <the.professor.tex@gmail.com>
2026-09-13 18:45:14 +0200

    update language paths and sitemap URLs for improved localization support

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -5,7 +5,7 @@ const escapeXml = (value: string) => value.replace(/[<>&'"]/g, (character) => ({
 
 export const GET: APIRoute = ({ site }) => {
     const isIndexable = data.demo !== true && Boolean(site);
-    const urls = isIndexable && site ? [new URL('/', site).href, new URL('/en/', site).href] : [];
+    const urls = isIndexable && site ? [new URL('/es/', site).href, new URL('/en/', site).href] : [];
     const entries = urls.map((url) => `<url><loc>${escapeXml(url)}</loc></url>`).join('');
     const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${entries}</urlset>\n`;
 
